f22578ffc726e81010dec0a91c170ecd2d9cd6ee
reddivarinandana <nadanareddy1122@gmail.com>
2023-05-28 03:17:49 +0530

    input added

--- week_07/day_1/inputbox/input.js
+++ week_07/day_1/inputbox/input.js
@@ -1,10 +1,11 @@
 let button = document.querySelector(".button");
-button.onclick = function(){
-    let div = document.createElement("div");
-    let first = document.querySelector(".first");
-    let last = document.querySelector(".last");
-    let delay = document.querySelector(".delay");
-    button.append(first,last,delay);
-    // let display = document.querySelector(".display");
-    // display.append(div);
+function view(){
+    let first = document.querySelector(".first").value;
+    let last = document.querySelector(".last").value;
+    let delay = document.querySelector(".delay").value;
+
+    setTimeout(function(){
+        var name = first+last;
+        document.querySelector(".display").textContent = name;
+    },delay *  500);
 }
